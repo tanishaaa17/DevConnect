9be030fc82c4932560d0651d8547cb4580644abc
tanishaaa17 <139698819+tanishaaa17@users.noreply.github.com>
2025-07-10 14:01:23 +0530

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to DevConnect 👋</h1>
+      <p>This is a placeholder for the developer social platform frontend.</p>
+    </div>
+  );
+}
+
+export default App;
